5c465174a2321ddb9be340405e442082fcd95ac1
Ryan B <admirallasers@gmail.com>
2016-09-19 21:35:25 -0500

    pushing up classwork

---
+++ SavingVariables.Tests/VariableTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavingVariables.DAL;
+using Moq;
+using SavingVariables.Models;
+using System.Collections.Generic;
+
+namespace SavingVariables.Tests
+{
+    [TestClass]
+    public class VariableTests
+    {
+        Mock<VariableContext> mock_context { get; set; }
+        Mock<DbSet<Variable>> mock_variable_table { get; set; }
+        List<Variable> VariableList { get; set; }
+        VariableRepository repo { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<VariableContext>();
+            mock_variable_table = new Mock<DbSet<Variable>>();
+            VariableList = new List<Variable>();
+            repo = new VariableRepository(mock_context.Object);
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+            repo = null;
+        }
+
+        [TestMethod]
+        public void CreateInstanceOfVariableRepo()
+        {
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void RepoHasContext()
+        {
+            Assert.IsNotNull(repo.Context);
+        }
+    }
+}
---
+++ SavingVariables/DAL/VariableRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.DAL
+{
+    public class VariableRepository
+    {
+        public VariableContext Context { get; set; }
+        public VariableRepository(VariableContext context)
+        {
+            this.Context = context;
+        }
+    }
+}
