6eb639db5e087cda9cb57e955fd87cd8d26e9f17
Ryan B <admirallasers@gmail.com>
2016-09-21 20:17:30 -0500

    added unit tests & basic skeleton

--- SavingVariables/DAL/VariableRepository.cs
+++ SavingVariables/DAL/VariableRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SavingVariables.Models;
 
 namespace SavingVariables.DAL
 {
@@ -12,6 +13,31 @@ namespace SavingVariables.DAL
         public VariableRepository(VariableContext context)
         {
             this.Context = context;
+        }
+
+        public List<Variable> GetVariables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddVariable(Variable newVariable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Variable Find(string variableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Variable ClearVariable(string variableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
